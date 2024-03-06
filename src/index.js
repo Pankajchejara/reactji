c383d76fdf6d2c9024baba306a226989b3cd1624
pankajchejara <pankajkumarchejara119@gmail.com>
2024-03-06 23:05:47 +0530

    ch

--- src/index.js
+++ src/index.js
@@ -9,7 +9,7 @@ import Appcontextprovider from '../src/context/Appcontext'
 import { Toaster } from 'react-hot-toast';
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-  <Appcontextprovider basename="reactji">
+  <Appcontextprovider basename="/reactji/">
      <HashRouter >
     <App />
     <Toaster />
