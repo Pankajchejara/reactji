0fa17e0968492c7ecfd6c1b924ade4884c9eb158
pankajchejara <pankajkumarchejara119@gmail.com>
2024-03-07 09:24:43 +0530

    ch

--- src/index.js
+++ src/index.js
@@ -2,18 +2,18 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
 import "./index.css";
-// import {BrowserRouter} from 'react-router-dom';
-import { HashRouter } from "react-router-dom";
+import {BrowserRouter} from 'react-router-dom';
+// import { HashRouter } from "react-router-dom";
 
 import Appcontextprovider from '../src/context/Appcontext'
 import { Toaster } from 'react-hot-toast';
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <Appcontextprovider >
-     <HashRouter  >
+     <BrowserRouter  >
     <App />
     <Toaster />
-    </HashRouter>
+    </BrowserRouter>
   </Appcontextprovider>
  
 );
